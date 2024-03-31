3d3aaa711e4094920020b26dca5db8a5b434a44b
Qelemnaz90 <enazy1990@gmail.com>
2024-03-31 18:33:21 +0400

    index

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let input =document.querySelector('#toDoInput')
+let button = document.querySelector('#addButton')
+let list = document.querySelector('#toDoList')
+
+
+let arr = []
+input.addEventListener('keypress', function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        let a = input.value
+        input.value = ''
+        arr.push(a)
+    renderList()}
+})
+
+function renderList() {
+    list.innerHTML = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = `${arr[i]}<button class="delete">Delete</button>`;
+        listItem.querySelector('.delete').addEventListener('click', function() {
+            arr.splice(i, 1);
+            renderList();
+        });
+        list.appendChild(listItem);
+    }
+    console.log(arr);
+}
